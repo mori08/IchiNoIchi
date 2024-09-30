38d2bec1fbff6a8bb590e9376a807e0fddfc85cd
mori08 <mrmt0814@gmail.com>
2024-09-30 21:30:29 +0900

    feat: #4: Window設定

--- IchiNoIchi/IchiNoichi/src/Main.cpp
+++ IchiNoIchi/IchiNoichi/src/Main.cpp
@@ -3,18 +3,27 @@
 #include <IchiNoIchi/DividingBlockDrawer.hpp>
 #include <IchiNoIchi/Game/Game.hpp>
 
+namespace
+{
+	IchiNoIchi::Config config_window(U"Window");
+}
+
 void Main()
 {
-	// System
-	System::SetTerminationTriggers(UserAction::CloseButtonClicked);
-
-	// Scene
-	Scene::SetBackground(IchiNoIchi::MyWhite);
-
 	// IchiNoIchi
 	IchiNoIchi::registerResourceAndAsset();
 	IchiNoIchi::Config::load();
 	IchiNoIchi::Game game;
+
+	// System
+	System::SetTerminationTriggers(UserAction::CloseButtonClicked);
+
+	// Window
+	Window::Resize(config_window.get<Point>(U"size"));
+	Window::SetTitle(config_window.get<String>(U"title"));
+
+	// Scene
+	Scene::SetBackground(IchiNoIchi::MyWhite);
 
 	while (System::Update())
 	{
